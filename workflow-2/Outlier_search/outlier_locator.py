8441546ae4e1b7405b166453672440e46e669594
Hyungro Lee <hroe.lee@gmail.com>
2020-03-15 16:48:20 -0400

    workflow-2: syntax for py3

--- workflow-2/Outlier_search/outlier_locator.py
+++ workflow-2/Outlier_search/outlier_locator.py
@@ -52,7 +52,7 @@ for i in range(len(model_weights)):
         if loss < loss_model_best:
             model_best, loss_model_best = model_weights[i], loss
 
-print "Using model {} with loss {}".format(model_best, loss_model_best) 
+print ("Using model {} with loss {}".format(model_best, loss_model_best))
     
 # Convert everything to cvae input 
 cm_data_lists = [read_h5py_file(cm_file) for cm_file in cm_files_list] 
@@ -78,7 +78,7 @@ else:
 # for model_weight in model_weights: 
 # Identify the latent dimensions 
 model_dim = int(os.path.basename(os.path.dirname(model_best))[10:12]) 
-print 'Model latent dimension: %d' % model_dim  
+print ('Model latent dimension: %d' % model_dim )
 # Get the predicted embeddings 
 cm_predict = predict_from_cvae(model_best, cvae_input, hyper_dim=model_dim) 
 # initialize eps if empty 
@@ -107,13 +107,13 @@ with open(eps_record_filepath, 'w') as eps_file:
         json.dump(eps_record, eps_file) 
 
 if DEBUG: 
-    print outlier_list_uni
+    print (outlier_list_uni)
     
 
 # Write the outliers using MDAnalysis 
 outliers_pdb_path = os.path.abspath('./outlier_pdbs') 
 make_dir_p(outliers_pdb_path) 
-print 'Writing outliers in %s' % outliers_pdb_path  
+print ('Writing outliers in %s' % outliers_pdb_path)
 
 new_outliers_list = [] 
 for outlier in outlier_list_uni: 
@@ -121,16 +121,18 @@ for outlier in outlier_list_uni:
     outlier_pdb_file = os.path.join(outliers_pdb_path, '{}_{:06d}.pdb'.format(os.path.basename(os.path.dirname(traj_file)), num_frame)) 
     # Only write new pdbs to reduce redundancy. 
     if not os.path.exists(outlier_pdb_file): 
-        print 'Found a new outlier# {} at frame {} of {}'.format(outlier, num_frame, traj_file)
+        print ('Found a new outlier# {} at frame {} of {}'.format(outlier,
+            num_frame, traj_file))
         outlier_pdb = write_pdb_frame(traj_file, pdb_file, num_frame, outlier_pdb_file)  
-        print '     Written as {}'.format(outlier_pdb_file)
+        print ('     Written as {}'.format(outlier_pdb_file))
     new_outliers_list.append(outlier_pdb_file) 
 
 # Clean up outdated outliers 
 outliers_list = glob(os.path.join(outliers_pdb_path, 'omm_runs*.pdb')) 
 for outlier in outliers_list: 
     if outlier not in new_outliers_list: 
-        print 'Old outlier {} is now connected to a cluster and removing it from the outlier list '.format(os.path.basename(outlier))
+        print ('Old outlier {} is now connected to a cluster and removing it
+        from the outlier list '.format(os.path.basename(outlier)))
         os.rename(outlier, os.path.join(os.path.dirname(outlier), '_'+os.path.basename(outlier))) 
 
 
@@ -148,16 +150,17 @@ for checkpnt in checkpnt_list:
     checkpnt_filepath = os.path.join(outliers_pdb_path, os.path.basename(os.path.dirname(checkpnt) + '.chk'))
     if not os.path.exists(checkpnt_filepath): 
         shutil.copy2(checkpnt, checkpnt_filepath) 
-        print [os.path.basename(os.path.dirname(checkpnt)) in outlier for outlier in outliers_list] 
+        print ([os.path.basename(os.path.dirname(checkpnt)) in outlier for
+            outlier in outliers_list])
         if any(os.path.basename(os.path.dirname(checkpnt)) in outlier for outlier in outliers_list):  
             restart_checkpnts.append(checkpnt_filepath) 
 
 if DEBUG: 
-    print restart_checkpnts
+    print (restart_checkpnts)
 
 
 if DEBUG: 
-    print restart_pdbs
+    print (restart_pdbs)
 
 # rank the restart_pdbs according to their RMSD to local state 
 if ref_pdb_file: 
@@ -174,7 +177,7 @@ else:
 
 # Write record for next step 
 restart_points = restart_checkpnts + restart_pdbs
-print restart_points 
+print (restart_points)
 
 restart_points_filepath = os.path.abspath('./restart_points.json') 
 with open(restart_points_filepath, 'w') as restart_file: 
