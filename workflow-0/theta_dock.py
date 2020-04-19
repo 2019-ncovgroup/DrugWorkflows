be68302e92eb94f9b857ee8b26289caef7c1ec62
Austin Clyde <aclyde11@gmail.com>
2020-04-18 20:55:10 -0500

    Update theta_dock.py
    changed csv interface

--- workflow-0/theta_dock.py
+++ workflow-0/theta_dock.py
@@ -13,8 +13,7 @@ from impress_md import interface_functions
 if __name__ == '__main__':
 
     assert('OE_LICENSE' in os.environ)
-
-    smiles_files = pd.read_csv(sys.argv[1], sep=' ', header=None)
+    smiles_file_name = sys.argv[1]
     target_filoe = sys.argv[2]
     start_idx    = int(sys.argv[3])
     n_smiles     = int(sys.argv[4])
@@ -24,14 +23,19 @@ if __name__ == '__main__':
     high_resolution = True
     
     pdb_name = re.sub("_receptor.oeb", "", target_filoe).split("/")[-1]
+       
+    smiles_files = pd.read_csv(smiles_file_name)
+    smiles_col = int(np.where(['smile' in s.lower() for s in smiles_files.columns.tolist()])[0][0])
+    name_col = int(
+        np.where(['cid' in s.lower() or 'title' in s.lower() or "name" in s.lower() for s in smiles_files.columns.tolist()])[0][0])
     
     
     docker, receptor = interface_functions.get_receptr(target_filoe, has_ligand=has_ligand, high_resolution=high_resolution)
 
     for pos in range(start_idx, start_idx + n_smiles):
 
-        smiles      = smiles_files.iloc[pos, 0]
-        ligand_name = smiles_files.iloc[pos, 1]
+        smiles      = smiles_files.iloc[pos, smiles_col]
+        ligand_name = smiles_files.iloc[pos, name_col]
         _, res = interface_functions.RunDocking_(smiles,
                                                 dock_obj=docker,
                                                 pos=pos,
