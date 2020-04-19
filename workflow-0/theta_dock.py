973176436f8323212f2c61ad0a4f6bfd185585ce
Austin Clyde <aclyde11@gmail.com>
2020-04-18 20:52:46 -0500

    Update theta_dock.py
    updated for new parameters

--- workflow-0/theta_dock.py
+++ workflow-0/theta_dock.py
@@ -19,17 +19,25 @@ if __name__ == '__main__':
     start_idx    = int(sys.argv[3])
     n_smiles     = int(sys.argv[4])
     has_ligand   = bool(sys.argv[5])
+    
+    force_flipper = True
+    high_resolution = True
+    
     pdb_name = re.sub("_receptor.oeb", "", target_filoe).split("/")[-1]
     
-
-    docker, receptor = interface_functions.get_receptr(target_filoe, has_ligand=has_ligand)
+    
+    docker, receptor = interface_functions.get_receptr(target_filoe, has_ligand=has_ligand, high_resolution=high_resolution)
 
     for pos in range(start_idx, start_idx + n_smiles):
 
         smiles      = smiles_files.iloc[pos, 0]
         ligand_name = smiles_files.iloc[pos, 1]
-        score, res  = interface_functions.RunDocking_(
-                smiles, docker, pos=pos, name=ligand_name, target_name=pdb_name)
+        _, res = interface_functions.RunDocking_(smiles,
+                                                dock_obj=docker,
+                                                pos=pos,
+                                                name=ligand_name,
+                                                target_name=pdb_name,
+                                                force_flipper=force_flipper)
         print(pos, res, end='')
 
 
