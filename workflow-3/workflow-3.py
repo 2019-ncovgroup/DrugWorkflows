b608e1c6bed13589dcbff00df98f3cf1ec805c98
Li Tan <darkwhite29@gmail.com>
2020-04-21 17:52:38 -0400

    Add files via upload
    final version

--- workflow-3/workflow-3.py
+++ workflow-3/workflow-3.py
@@ -1,6 +1,6 @@
 from radical import entk
 import os
-import argparse, sys
+import argparse, sys, math
 
 class ESMACS(object):
 
@@ -21,32 +21,35 @@ class ESMACS(object):
 
     def set_argparse(self):
         parser = argparse.ArgumentParser(description="ESMACS")
-        parser.add_argument("--task", "-t", help="sim_esmacs, esmacs, or esmacs_analysis")
+        parser.add_argument("--task", "-t", help="sim_esmacs or esmacs")
+        parser.add_argument("--structures", "-i", help="Path to input files")
+        parser.add_argument("--trajectory", "-o", help="Path to output files (default: same as input path)")
+        parser.add_argument("--nanoseconds", "-n", default=6, help="Simulation length (default: 6)")
+        parser.add_argument("--replicas", "-r", default=24, help="Ensemble size for ESMACS (default: 24)")
+        parser.add_argument("--component", "-c", default="com", \
+                help="Component (default: com, complex=com, protein=apo and" \
+                " ligand=lig)")
         args = parser.parse_args()
         self.args = args
-        if args.task is None:
+        if args.task is None or args.structures is None:
             parser.print_help()
             sys.exit(-1)
+        elif args.trajectory is None:
+            args.trajectory = args.structures
 
-    def sim_esmacs_py(self, rep_count=24):
+    def sim_esmacs_py(self, rep_count=24, structures=None, trajectory=None,
+                      component="com", nanoseconds=6):
 
-        for i in range(1, rep_count + 1):
+        for i in range(1, int(rep_count) + 1):
             t = entk.Task()
             pre_exec = """export OMP_NUM_THREADS=1
-                export INPATH="$MEMBERWORK/med110/inpath"
-                . /ccs/home/litan/miniconda3/etc/profile.d/conda.sh
-                conda activate wf3
-                module load cuda/10.1.243 gcc/7.4.0 spectrum-mpi/10.3.1.2-20200121
-                mkdir -p $INPATH; cd $INPATH"""
+                export WDIR="$MEMBERWORK/chm155/Model-generation"
+                . /gpfs/alpine/scratch/apbhati/chm155/miniconda/etc/profile.d/conda.sh
+                conda activate conda-entk
+                module load cuda gcc spectrum-mpi"""
             t.pre_exec = [x.strip() for x in pre_exec.split("\n")]
-            t.pre_exec += [
-                "export OUTPATH=$INPATH",#"export OUTPATH=\"$INPATH/rep{}\"".format(i),
-                "mkdir -p $OUTPATH; cd $OUTPATH",
-                "rm -f traj.dcd sim.log"
-            ]
-
-            t.executable = '/ccs/home/litan/miniconda3/envs/wf3/bin/python3.7'
-            t.arguments = ['$INPATH/sim_esmacs.py', '-i$INPATH', '-n3', '-r{}'.format(i)]
+            t.executable = '/gpfs/alpine/scratch/apbhati/chm155/miniconda/envs/conda-entk/bin/python'
+            t.arguments = ['$WDIR/sim_esmacs.py', '-i{}'.format(structures), '-o{}'.format(trajectory), '-n{}'.format(nanoseconds), '-c{}'.format(component), '-r{}'.format(i)]
             t.post_exec = []
 
             t.cpu_reqs = {
@@ -66,34 +69,55 @@ class ESMACS(object):
 
         self.p.add_stages(self.s)
 
-    def esmacs_py(self, rep_count=24):#mmpbsa(self):
+    def esmacs_py(self, rep_count=24, component="com", structures=None,
+            trajectory=None, nanoseconds=6):
 
-        for i in range(1, rep_count + 1):
+        for i in range(1, int(rep_count) + 1):
             t = entk.Task()
             t.pre_exec = [ 
                     "export OMP_NUM_THREADS=1",
-                    ". /ccs/home/litan/miniconda2/etc/profile.d/conda.sh",
-                    "conda activate wf3",
-                    #"conda clean --packages",
-                    #"conda install -y cython matplotlib",
-                    #"pip install docopt futures --user",
-                    "module load cuda/10.1.243 gcc/7.4.0 spectrum-mpi/10.3.1.2-20200121",
+                    ". /gpfs/alpine/scratch/apbhati/chm155/miniconda/etc/profile.d/conda.sh",
+                    "conda activate conda-entk",
+                    "module load cuda/10.1.243 gcc/6.4.0 spectrum-mpi/10.3.1.2-20200121",
                     "export CUDA_HOME=/sw/summit/cuda/10.1.243",
-                    "export INPATH=\"$MEMBERWORK/med110/inpath\"", # TODO: parameterized
-                    "source /ccs/home/litan/DrugWorkflows/workflow-3/amber18/amber.sh", #TODO: parameterized
+                    "export WDIR=\"$MEMBERWORK/chm155/Model-generation\"", 
+                    "source /gpfs/alpine/scratch/apbhati/chm155/AmberTools19/amber18/amber.sh", 
                     "export LD_LIBRARY_PATH=\"/sw/summit/cuda/10.1.243/lib:${LD_LIBRARY_PATH}\"",
-                    "cd $INPATH",
-                    "export OUTPATH=$INPATH/com/rep{}".format(i),
-                    "cd $OUTPATH"
+                    "export INPATH={}".format(structures),
+                    "export COM={}".format(component),
+                    "cd {}/{}/rep{}".format(trajectory, component, i)
                     ]
-
-            t.executable = '/ccs/home/litan/miniconda3/envs/wf3/bin/python3.7'
-            t.arguments = ['$INPATH/esmacs.py', '-i$INPATH', '-r{}'.format(i)]
-            t.post_exec = []
+            # Amber
+            t.executable = "MMPBSA.py.MPI"
+            if component == "com":
+                t.arguments = ("-O -i $WDIR/impress_md/esmacs.in -sp " + \
+                        "$INPATH/${COM}_sol.prmtop -cp $INPATH/com.prmtop -rp " + \
+                        "$INPATH/apo.prmtop -lp $INPATH/lig.prmtop -y traj.dcd " + \
+                        "> mmpbsa.log").split()
+                post_exec = """ls _MMPBSA_complex_gb.mdout.* | sort -V | xargs cat > _MMPBSA_complex_gb.mdout.all
+                ls _MMPBSA_complex_gb_surf.dat.* | sort -V | xargs cat > _MMPBSA_complex_gb_surf.dat.all
+                ls _MMPBSA_complex_pb.mdout.* | sort -V | xargs cat > _MMPBSA_complex_pb.mdout.all
+                ls _MMPBSA_ligand_gb.mdout.* | sort -V | xargs cat > _MMPBSA_ligand_gb.mdout.all
+                ls _MMPBSA_ligand_gb_surf.dat.* | sort -V | xargs cat > _MMPBSA_ligand_gb_surf.dat.all
+                ls _MMPBSA_ligand_pb.mdout.* | sort -V | xargs cat > _MMPBSA_ligand_pb.mdout.all
+                ls _MMPBSA_receptor_gb.mdout.* | sort -V | xargs cat > _MMPBSA_receptor_gb.mdout.all
+                ls _MMPBSA_receptor_gb_surf.dat.* | sort -V | xargs cat > _MMPBSA_receptor_gb_surf.dat.all
+                ls _MMPBSA_receptor_pb.mdout.* | sort -V | xargs cat > _MMPBSA_receptor_pb.mdout.all
+                rm _MMPBSA_*.[0-9]* reference.frc *.inpcrd *.mdin* *.out"""
+            else:
+                t.arguments = ("-O -i $WDIR/impress_md/esmacs.in -sp " + \
+                        "$INPATH/${COM}_sol.prmtop -cp $INPATH/$COM.prmtop " + \
+                        "-y traj.dcd > mmpbsa.log").split()
+                post_exec = """ls _MMPBSA_complex_gb.mdout.* | sort -V | xargs cat > _MMPBSA_complex_gb.mdout.all
+                ls _MMPBSA_complex_gb_surf.dat.* | sort -V | xargs cat > _MMPBSA_complex_gb_surf.dat.all
+                ls _MMPBSA_complex_pb.mdout.* | sort -V | xargs cat > _MMPBSA_complex_pb.mdout.all
+                rm _MMPBSA_*.[0-9]* reference.frc *.inpcrd *.mdin* *.out"""
+            t.post_exec = [ x.strip() for x in post_exec.split("\n") ]
+            t.post_exec = ["cd {}/{}/rep{}".format(trajectory, component, i)] + t.post_exec
 
             t.cpu_reqs = {
-                    'processes': 1,
-                    'process_type': None,
+                    'processes': (int(nanoseconds)-2)*10,
+                    'process_type': 'MPI',
                     'threads_per_process': 4,
                     'thread_type': 'OpenMP'
                     }
@@ -108,26 +132,6 @@ class ESMACS(object):
 
         self.p.add_stages(self.s)
 
-    def esmacs_analysis_py(self, energy_files, replicas=None, traj_type="one"):
-
-        t = entk.Task()
-        t.executable = 'python'
-        t.arguments = [ 'esmacs_analysis.py', '-i={}'.format(energy_files) ]
-        if replicas:
-            t.arguments += [ '-r={}'.format(replicas) ]
-        t.arguments += [ '-t={}'.format(traj_type) ]
-        
-        t.cpu_reqs = {
-                    'processes': 1,
-                    'process_type': None,
-                    'threads_per_process': 4,
-                    'thread_type': 'OpenMP'
-                    }
-
-        self.s.add_tasks(t)
-
-        self.p.add_stages(self.s)
-
     def run(self):
         self.am.workflow = [self.p]
         self.am.run()
@@ -139,42 +143,37 @@ if __name__ == "__main__":
 
     if esmacs.args.task == "sim_esmacs":
 
-        n_nodes = 4
+        n_nodes = math.ceil(float(int(esmacs.args.replicas)/6))
         esmacs.set_resource(res_desc = {
             'resource': 'ornl.summit',
             'queue'   : 'batch',
-            'walltime': 120, #MIN
+            'walltime': 10, #MIN
             'cpus'    : 168 * n_nodes,
             'gpus'    : 6 * n_nodes,
-            'project' : 'MED110'
+            'project' : 'CHM155_001'
             })
-        esmacs.sim_esmacs_py(rep_count=24)
+        esmacs.sim_esmacs_py(rep_count=esmacs.args.replicas,
+                component=esmacs.args.component,
+                structures=esmacs.args.structures,
+                nanoseconds=esmacs.args.nanoseconds,
+                trajectory=esmacs.args.trajectory)
         esmacs.run()
 
     elif esmacs.args.task == "esmacs":
 
-        n_nodes = 24
+        n_nodes = int(esmacs.args.replicas)
         esmacs.set_resource(res_desc = {
             'resource': 'ornl.summit',
             'queue'   : 'batch',
-            'walltime': 10, #MIN
+            'walltime': 5, #MIN
             'cpus'    : 168 * n_nodes,
             'gpus'    : 6 * n_nodes,
-            'project' : 'MED110'
+            'project' : 'CHM155_001'
             })
-        esmacs.esmacs_py(rep_count=24)
+        esmacs.esmacs_py(rep_count=esmacs.args.replicas, 
+                component=esmacs.args.component,
+                structures=esmacs.args.structures,
+                nanoseconds=esmacs.args.nanoseconds,
+                trajectory=esmacs.args.trajectory)
         esmacs.run()
 
-    elif esmacs.args.task == "esmacs_analysis":
-
-        n_nodes = 1
-        esmacs.set_resource(res_desc = {
-            'resource': 'ornl.summit',
-            'queue'   : 'batch',
-            'walltime': 10, #MIN
-            'cpus'    : 168 * n_nodes,
-            'gpus'    : 6 * n_nodes,
-            'project' : "MED110"
-            })
-        esmacs.esmacs_analysis_py()
-        esmacs.run()
