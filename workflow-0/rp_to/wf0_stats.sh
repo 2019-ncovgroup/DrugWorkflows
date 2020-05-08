54767bedfae8c50e0e957ad6326ee763186f2ade
Andre Merzky <andre@merzky.net>
2020-05-08 11:43:56 -0500

    script updates

--- workflow-0/rp_to/wf0_stats.sh
+++ workflow-0/rp_to/wf0_stats.sh
@@ -1,33 +1,60 @@
 #!/bin/sh
 
 total=$1
-count=$(grep 'result_cb request' un*/*OUT | wc -l)
-now=$(date +%s)
-start=$(cat un*/un*prof | grep cu_exec_start | cut -f 1 -d , | sort -n | head -n 1)
-stop=$( cat un*/un*prof | grep cu_exec_stop  | cut -f 1 -d , | sort -n | tail -n 1)
+sum=0
+for p in pilot.*; do
 
-test -z "$total" && total=6700000
-test -z "$stop"  && stop=$now
+    check=$(ls $p/unit.*/STDOUT 2>/dev/null)
+    slurmid=$(grep SLURM_JOB_ID $p/env.orig | cut -f 2 -d '"')
 
-run=$( echo "$stop  - $start"       | bc -l)
-rate=$(echo "$count / $run"         | bc -l)
-part=$(echo "$count / $total * 100" | bc -l)
-est=$( echo "$run   / $part  * 100" | bc -l)
-rem=$( echo "$est   - $run"         | bc -l)
+    if test -z "$check"
+    then
+        receptor=$(grep receptor $p/unit.*/wf0.cfg | head -n 1 | cut -f 4 -d '"')
+        printf          "\n"
+        printf "receptor:    %10s\n"           $receptor
 
-printf "smiles  : %10d\n"           $total
-printf "count   : %10d [%4.1f%%]\n" $count $part
-printf "rate    : %10.1f /sec\n"    $(echo "$count / $run"           | bc -l)
-printf "          %10.1f /min\n"    $(echo "$count / $run * 60"      | bc -l)
-printf "          %10.1f /hrs\n"    $(echo "$count / $run * 60 * 60" | bc -l)
-printf "runtime : %10.1f sec\n"     $(echo "$run"                    | bc -l)
-printf "          %10.1f min\n"     $(echo "$run          / 60"      | bc -l)
-printf "          %10.1f hrs\n"     $(echo "$run          / 60 / 60" | bc -l)
-printf "estimate: %10.1f sec\n"     $(echo "$est"                    | bc -l)
-printf "          %10.1f min\n"     $(echo "$est          / 60"      | bc -l)
-printf "          %10.1f hrs\n"     $(echo "$est          / 60 / 60" | bc -l)
-printf "remain  : %10.1f sec\n"     $(echo "$rem"                    | bc -l)
-printf "          %10.1f min\n"     $(echo "$rem          / 60"      | bc -l)
-printf "          %10.1f hrs\n"     $(echo "$rem          / 60 / 60" | bc -l)
+    else
+        receptor=$(grep receptor $p/unit.*/wf0.cfg | head -n 1 | cut -f 4 -d '"')
+        count=$(grep 'result_cb request' $p/un*/*OUT | wc -l)
+        now=$(date +%s)
+        start=$(cat $p/un*/un*prof | grep cu_exec_start | cut -f 1 -d , | sort -n | head -n 1)
+        stop=$( cat $p/un*/un*prof | grep cu_exec_stop  | cut -f 1 -d , | sort -n | tail -n 1)
+        
+        test -z "$total" && total=6700000
+        test -z "$stop"  && stop=$now
+        
+        run=$( echo "$stop  - $start"       | bc -l)
+        rate=$(echo "$count / $run"         | bc -l)
+        part=$(echo "$count / $total * 100" | bc -l)
+        est=$( echo "$run   / $part  * 100" | bc -l)
+        rem=$( echo "$est   - $run"         | bc -l)
+        
+        skip="                                          "
+        printf "receptor:    %-30s"                  $receptor
+        printf "smiles  :    %10d"                   $total
+        printf                "    %10d [%5.1f%%]"   $((total - count)) $(echo "100 - $part" | bc -l)
+        printf                "    %10d [%5.1f%%]\n" $count $part
+        printf "             %-10s  %8s          "   $p $slurmid
+        printf       "rate    :    %10.1f /sec"      $(echo "$count / $run"           | bc -l)
+        printf                "    %10.1f /min"      $(echo "$count / $run * 60"      | bc -l)
+        printf                "    %10.1f /hrs\n"    $(echo "$count / $run * 60 * 60" | bc -l)
+        printf "$skip runtime :    %10.1f  sec"      $(echo "$run"                    | bc -l)
+        printf                "    %10.1f  min"      $(echo "$run          / 60"      | bc -l)
+        printf                "    %10.1f  hrs\n"    $(echo "$run          / 60 / 60" | bc -l)
+        if ! test "$rem" = 0
+        then
+            printf "$skip estimate:    %10.1f  sec"      $(echo "$est"                    | bc -l)
+            printf                "    %10.1f  min"      $(echo "$est          / 60"      | bc -l)
+            printf                "    %10.1f  hrs\n"    $(echo "$est          / 60 / 60" | bc -l)
+            printf "$skip remain  :    %10.1f  sec"      $(echo "$rem"                    | bc -l)
+            printf                "    %10.1f  min"      $(echo "$rem          / 60"      | bc -l)
+            printf                "    %10.1f  hrs\n"    $(echo "$rem          / 60 / 60" | bc -l)
+            rate=$(echo "$count / $run" | bc -l)
+            sum=$(echo "$sum + $rate"   | bc -l)
+        fi
 
+    fi
+
+done
+printf "total rate: %10.1f\n" $sum
 
