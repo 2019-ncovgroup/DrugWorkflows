b0738c342271a8ee7715a9b41e449538437789d0
Mikhail Titov <mikhail.a.titov@gmail.com>
2020-05-28 17:26:51 -0400

    added Lassen related files to run WF2

---
+++ workflow-2/lassen_adrp.py
@@ -0,0 +1,343 @@
+import glob
+import json
+import os
+import time
+
+from radical.entk import Pipeline, Stage, Task, AppManager
+
+#
+# [1] Lassen: https://hpc.llnl.gov/software/banks-running-jobs
+# [2] Workflow: https://docs.google.com/document/d/1XFgg4rlh7Y2nckH0fkiZTxfauadZn_zSn3sh51kNyKE/
+#
+
+# ------------------------------------------------------------------------------
+# Set default verbosity
+
+if os.environ.get('RADICAL_ENTK_VERBOSE') is None:
+    os.environ['RADICAL_ENTK_REPORT'] = 'True'
+'''
+export RADICAL_PROFILE=True
+export RADICAL_PILOT_PROFILE=True
+export RADICAL_ENTK_PROFILE=True
+'''
+
+base_path = os.path.abspath('.')
+
+md_path = os.path.join(base_path, 'MD_exps/adrp')
+agg_path = os.path.join(base_path, 'MD_to_CVAE')
+cvae_path = os.path.join(base_path, 'CVAE_exps')
+param_path = os.path.join(base_path, 'Parameters')
+outlier_path = os.path.join(base_path, 'Outlier_search')
+
+input_path = sorted(glob.glob(os.path.join(param_path, 'input_*')))
+input_prot_path = [path for path in input_path if 'rank' not in path]
+input_comp_path = [path for path in input_path if 'rank' in path]
+n_comp = len(input_comp_path)
+
+N_jobs_MD = 120
+N_jobs_ML = 12
+
+resource_name = 'llnl.lassen'
+proj_id = 'cv19-a01'
+queue = 'pbatch'
+hrs_wt = 24
+
+CUR_STAGE = 0
+MAX_STAGE = 2
+RETRAIN_FREQ = 5
+
+LEN_initial = 100
+LEN_iter = 10
+
+batch_size = 256
+
+TASK_PRE_EXEC_MODULES = [
+    'module load cuda/10.2.89',
+    'source %s/amber.sh' % os.environ.get('AMBERHOME')]
+
+
+def generate_training_pipeline():
+    """
+    Function to generate the CVAE_MD pipeline
+    """
+
+    def generate_MD_stage(num_MD=1):
+        """
+        Function to generate MD stage.
+        """
+        s1 = Stage()
+        s1.name = 'MD'
+        initial_MD = True
+        outlier_filepath = '%s/restart_points.json' % outlier_path
+
+        if os.path.exists(outlier_filepath):
+            initial_MD = False
+            outlier_file = open(outlier_filepath, 'r')
+            outlier_list = json.load(outlier_file)
+            outlier_file.close()
+
+            # MD tasks
+        time_stamp = int(time.time())
+        for i in range(num_MD):
+            t1 = Task()
+            # https://github.com/radical-collaboration/hyperspace/blob/MD/microscope/experiments/MD_exps/fs-pep/run_openmm.py
+
+            t1.pre_exec = TASK_PRE_EXEC_MODULES[:]
+            t1.pre_exec += ['export' + \
+                            ' PYTHONPATH=%s/MD_exps:%s/MD_exps/MD_utils:$PYTHONPATH' %
+                            (base_path, base_path)]
+            t1.pre_exec += ['cd %s' % md_path]
+            t1.executable = ['python']  # run_openmm.py
+            t1.arguments = ['%s/run_openmm.py' % md_path]
+
+            # specify the pdb_file and top_file for each run
+            # pick initial point of simulation
+            if initial_MD or i >= len(outlier_list):
+                pdb_file = os.path.join(input_prot_path[0], 'prot.pdb')
+                top_file = os.path.join(input_prot_path[0], 'prot.prmtop')
+                if i % 2 == 0:
+                    pdb_file = os.path.join(input_prot_path[0], 'prot.pdb')
+                    top_file = os.path.join(input_prot_path[0], 'prot.prmtop')
+                else:
+                    pdb_file = os.path.join(input_comp_path[i // 2 % n_comp], 'comp.pdb')
+                    top_file = os.path.join(input_comp_path[i // 2 % n_comp], 'comp.prmtop')
+                    # Outlier situation with restarting simulation from pdb file
+            elif outlier_list[i].endswith('pdb'):
+                pdb_file = outlier_list[i]
+                sim_path = os.path.join(md_path,
+                                        os.path.basename(pdb_file)[:-11])
+                top_file = glob.glob(sim_path + '/*top')[0]
+                # Restarting simulation with check point
+            elif outlier_list[i].endswith('chk'):
+                check_point = outlier_list[i]
+                t1.arguments += ['-c', check_point]
+                sim_path = os.path.join(md_path,
+                                        os.path.basename(check_point)[:-4])
+                pdb_file = glob.glob(sim_path + '/*pdb')[0]
+                top_file = glob.glob(sim_path + '/*top')[0]
+
+            # add pdb_file and top_file to simulation
+            ## identify the topology label
+            top_dirname = os.path.basename(os.path.dirname(top_file))
+            if 'omm_runs' in top_dirname:
+                top_label = top_dirname.split('_')[2]
+            elif 'input' in top_dirname:
+                top_label = top_dirname.split('_')[1]
+            else:
+                raise Exception('Unknow topology file...')
+
+                # make a copy of everything at local dir
+            work_dirname = 'omm_runs_%s_%d' % (top_label, time_stamp + i)
+            t1.pre_exec += ['mkdir -p {0} && cd {0}'.format(work_dirname)]
+            t1.pre_exec += ['cp %s ./' % pdb_file]
+            t1.pre_exec += ['cp %s ./' % top_file]
+            if '-c' in t1.arguments:
+                t1.pre_exec += ['cp %s ./' % check_point]
+
+            # addd file to simulation
+            t1.arguments += ['--pdb_file', pdb_file]
+            t1.arguments += ['--topol', top_file]
+
+            # how long to run the simulation
+            if initial_MD:
+                t1.arguments += ['--length', LEN_initial]
+            else:
+                t1.arguments += ['--length', LEN_iter]
+
+            # assign hardware the task
+            t1.cpu_reqs = {'processes': 1,
+                           'process_type': None,
+                           'threads_per_process': 4,
+                           'thread_type': 'OpenMP'
+                           }
+            t1.gpu_reqs = {'processes': 1,
+                           'process_type': None,
+                           'threads_per_process': 1,
+                           'thread_type': 'CUDA'
+                           }
+
+            # Add the MD task to the simulating stage
+            s1.add_tasks(t1)
+        return s1
+
+    def generate_aggregating_stage():
+        """
+        Function to concatenate the MD trajectory (h5 contact map)
+        """
+        s2 = Stage()
+        s2.name = 'aggregating'
+
+        # Aggregation task
+        t2 = Task()
+        # https://github.com/radical-collaboration/hyperspace/blob/MD/microscope/experiments/MD_to_CVAE/MD_to_CVAE.py
+        t2.pre_exec = []
+        t2.pre_exec += ['cd %s' % agg_path]
+        t2.executable = ['python']  # MD_to_CVAE.py
+        t2.arguments = ['%s/MD_to_CVAE.py' % agg_path,
+                        '--sim_path', md_path,
+                        '--pad', 4]
+
+        # Add the aggregation task to the aggreagating stage
+        s2.add_tasks(t2)
+        return s2
+
+    def generate_ML_stage(num_ML=1):
+        """
+        Function to generate the learning stage
+        """
+        s3 = Stage()
+        s3.name = 'learning'
+
+        # learn task
+        time_stamp = int(time.time())
+        for i in range(num_ML):
+            t3 = Task()
+            # https://github.com/radical-collaboration/hyperspace/blob/MD/microscope/experiments/CVAE_exps/train_cvae.py
+            t3.pre_exec = TASK_PRE_EXEC_MODULES[:]
+
+            t3.pre_exec += ['export ' + \
+                            'PYTHONPATH=%s/CVAE_exps:%s/CVAE_exps/cvae:$PYTHONPATH' %
+                            (base_path, base_path)]
+            t3.pre_exec += ['cd %s' % cvae_path]
+            dim = i + 3
+            cvae_dir = 'cvae_runs_%.2d_%d' % (dim, time_stamp + i)
+            t3.pre_exec += ['mkdir -p {0} && cd {0}'.format(cvae_dir)]
+            t3.executable = ['python']
+            t3.arguments = ['%s/train_cvae.py' % cvae_path,
+                            '--h5_file', '%s/cvae_input.h5' % agg_path,
+                            '--dim', dim,
+                            '--batch', batch_size]
+
+            t3.cpu_reqs = {'processes': 1,
+                           'process_type': None,
+                           'threads_per_process': 4,
+                           'thread_type': 'OpenMP'
+                           }
+            t3.gpu_reqs = {'processes': 1,
+                           'process_type': None,
+                           'threads_per_process': 1,
+                           'thread_type': 'CUDA'
+                           }
+
+            # Add the learn task to the learning stage
+            s3.add_tasks(t3)
+
+        return s3
+
+    def generate_interfacing_stage():
+        s4 = Stage()
+        s4.name = 'scanning'
+
+        # Scaning for outliers and prepare the next stage of MDs
+        t4 = Task()
+        t4.pre_exec = TASK_PRE_EXEC_MODULES[:]
+
+        t4.pre_exec += ['export ' + \
+                        'PYTHONPATH=%s/CVAE_exps:%s/CVAE_exps/cvae:$PYTHONPATH' %
+                        (base_path, base_path)]
+        t4.pre_exec += ['cd %s/Outlier_search' % base_path]
+        t4.executable = ['python']
+        t4.arguments = ['outlier_locator.py',
+                        '--md', md_path,
+                        '--cvae', cvae_path,
+                        ]
+
+        t4.cpu_reqs = {'processes': 1,
+                       'process_type': None,
+                       'threads_per_process': 12,
+                       'thread_type': 'OpenMP'
+                       }
+        t4.gpu_reqs = {'processes': 1,
+                       'process_type': None,
+                       'threads_per_process': 1,
+                       'thread_type': 'CUDA'
+                       }
+        s4.add_tasks(t4)
+        s4.post_exec = func_condition
+
+        return s4
+
+    def func_condition():
+        global CUR_STAGE, MAX_STAGE
+        if CUR_STAGE < MAX_STAGE:
+            func_on_true()
+        else:
+            func_on_false()
+
+    def func_on_true():
+        global CUR_STAGE, MAX_STAGE
+
+        # if CUR_STAGE != 0:
+        # --------------------------
+        # MD stage
+        s1 = generate_MD_stage(num_MD=N_jobs_MD)
+        # Add simulating stage to the training pipeline
+        p.add_stages(s1)
+
+        if CUR_STAGE % RETRAIN_FREQ == 0:
+            # --------------------------
+            # Aggregate stage
+            s2 = generate_aggregating_stage()
+            # Add the aggregating stage to the training pipeline
+            p.add_stages(s2)
+
+            # --------------------------
+            # Learning stage
+            s3 = generate_ML_stage(num_ML=N_jobs_ML)
+            # Add the learning stage to the pipeline
+            p.add_stages(s3)
+
+        # --------------------------
+        # Outlier identification stage
+        s4 = generate_interfacing_stage()
+        p.add_stages(s4)
+
+        CUR_STAGE += 1
+
+    def func_on_false():
+        print('Done')
+
+    global CUR_STAGE
+
+    p = Pipeline()
+    p.name = 'MD_ML'
+
+    func_on_true()
+
+    return p
+
+
+if __name__ == '__main__':
+    # Create a dictionary to describe four mandatory keys:
+    # resource, walltime, cores and project
+    # resource is 'local.localhost' to execute locally
+    res_dict = {
+        'resource': resource_name,
+        'project': proj_id,
+        'queue': queue,
+        'schema': 'local',
+        'walltime': 60 * hrs_wt,
+        'cpus': N_jobs_MD // 4 * 40,
+        'gpus': N_jobs_MD
+    }
+
+    with open('creds_lassen.json') as f:
+        creds = json.load(f)
+    os.environ['RADICAL_PILOT_DBURL'] = creds['mongodb']['url']
+    # Create Application Manager
+    appman = AppManager(**creds['rabbitmq'])
+    appman.resource_desc = res_dict
+
+    p1 = generate_training_pipeline()
+    # p2 = generate_MDML_pipeline()
+
+    pipelines = []
+    pipelines.append(p1)
+    # pipelines.append(p2)
+
+    # Assign the workflow as a list of Pipelines to the Application Manager. In
+    # this way, all the pipelines in the list will execute concurrently.
+    appman.workflow = pipelines
+
+    # Run the Application Manager
+    appman.run()
---
+++ workflow-2/readme_lassen.md
@@ -0,0 +1,281 @@
+`updated on: 05-28-2020`
+
+# Lassen@LLNL
+
+## Login ([docs](https://lc.llnl.gov/confluence/display/LC/SSH+Guide+for+Livermore+Computing))
+```shell script
+ssh -l <lcusername> lassen.llnl.gov
+```
+GPFS work directory: `/p/gpfs1/$USER` (IBM's Spectrum Scale file system)
+Bank: `cv19-a01` ("shares")
+
+## Environment installation
+```shell script
+export PREFIX=/p/gpfs1/$USER
+
+wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-ppc64le.sh -O $PREFIX/miniconda.sh
+chmod +x $PREFIX/miniconda.sh
+$PREFIX/miniconda.sh -b -p $PREFIX/.miniconda3
+source $PREFIX/.miniconda3/bin/activate
+conda update -y -n base -c defaults conda
+conda create -y -n ve.rp python=3.7
+conda activate ve.rp
+
+# in case of "CondaHTTPError: HTTP 000 CONNECTION FAILED for url <url>"
+# use the following command: `conda config --set ssl_verify no`
+```
+WF2 packages (IBM Watson Machine Learning Community Edition | WML CE)
+```shell script
+conda config --add channels https://public.dhe.ibm.com/ibmdl/export/pub/software/server/ibm-ai/conda/
+conda install -y -c https://public.dhe.ibm.com/ibmdl/export/pub/software/server/ibm-ai/conda/ cython scikit-learn scipy matplotlib h5py tensorflow-gpu
+conda install -c conda-forge -y swig
+conda install -c anaconda -y pytables
+conda install -y keras
+
+# cuda that corresponds WML CE should be used later: `module load cuda/10.2.89`
+
+conda config --add channels omnia-dev/label/cuda101 \
+             --add channels omnia/label/cuda101
+conda install -y openmm=7.4.0=py37_cuda101_0
+
+# if there is an error with installation of `gsd` package, 
+# then `gcc` module should be loaded (`module load gcc/8.3.1`)
+# *) this might mess with paths, thus need to re-activate conda-env again
+pip install MDAnalysis MDAnalysisTests parmed
+
+# package `typing` should be uninstalled, so the built-in one will be in use
+# (causes an issue for python 3.7, and not for 3.6) 
+# pip uninstall -y typing
+```
+RCT installation
+```shell script
+pip install git+https://github.com/radical-cybertools/radical.utils.git@devel
+pip install git+https://github.com/radical-cybertools/radical.saga.git@devel
+pip install git+https://github.com/radical-cybertools/radical.pilot.git@hotfix/lassen
+pip install git+https://github.com/radical-cybertools/radical.entk.git@devel
+```
+ZMQ installation
+```shell script
+cd /p/gpfs1/$USER
+
+wget https://github.com/zeromq/libzmq/archive/v4.3.2.tar.gz
+tar zxf v4.3.2.tar.gz
+cd libzmq-4.3.2/
+./autogen.sh
+./configure --prefix=`pwd -P`/install
+make -j
+make install
+export LD_LIBRARY_PATH=`pwd -P`/install/lib:$LD_LIBRARY_PATH
+export LD_RUN_PATH=`pwd -P`/install/lib:$LD_RUN_PATH
+pip install --no-cache-dir --force-reinstall --ignore-installed --no-binary :all: --install-option="--zmq=`pwd -P`/install" pyzmq
+```
+```shell script
+(ve.rp) [:libzmq-4.3.2]$ ldd /p/gpfs1/$USER/.miniconda3/envs/ve.rp/lib/python3.7/site-packages/zmq/backend/cython/socket.cpython-37m-powerpc64le-linux-gnu.so | grep zmq
+	libzmq.so.5 => /p/gpfs1/$USER/libzmq-4.3.2/install/lib/libzmq.so.5 (0x00002000000c0000)
+```
+
+## RADICAL-Pilot setup
+User config dir: `mkdir -p $HOME/.radical/pilot/configs/`
+
+Resource config file: `$HOME/.radical/pilot/configs/resource_llnl.json`:
+```shell script
+{
+    "lassen": {
+        "description"                 : "",
+        "notes"                       : "",
+        "schemas"                     : ["local"],
+        "local"                       : 
+        {
+            "job_manager_hop"         : "fork://localhost/",
+            "job_manager_endpoint"    : "lsf://localhost/",
+            "filesystem_endpoint"     : "file://localhost/"
+        },
+        "cores_per_node"              : 40,
+        "gpus_per_node"               : 4,
+        "sockets_per_node"            : 2,
+        "default_queue"               : "pbatch",
+        "resource_manager"            : "LSF_SUMMIT",
+        "agent_scheduler"             : "CONTINUOUS",
+        "agent_spawner"               : "POPEN",
+        "agent_launch_method"         : "JSRUN",
+        "task_launch_method"          : "JSRUN",
+        "mpi_launch_method"           : "JSRUN",
+        "pre_bootstrap_0"             : [
+            "module load jsrun",
+            "source /p/gpfs1/$USER/.miniconda3/etc/profile.d/conda.sh"],
+        "default_remote_workdir"      : "/p/gpfs1/$USER",
+        "valid_roots"                 : ["/p/gpfs1/$USER",
+                                         "$HOME"],
+        "python_dist"                 : "anaconda",
+        "virtenv"                     : "ve.rp",
+        "virtenv_mode"                : "use",
+        "rp_version"                  : "installed"
+    }
+}
+```
+
+### Test example
+Executable:
+```shell script
+wget -q https://raw.githubusercontent.com/radical-cybertools/radical.pilot/devel/examples/hello_rp.sh
+chmod +x hello_rp.sh
+```
+Script file `run_hello_rp.py` to run:
+```python
+#!/usr/bin/env python3
+
+import radical.pilot as rp
+
+N_TASKS = 10
+
+
+if __name__ == '__main__':
+    session = rp.Session()
+    try:
+        pmgr = rp.PilotManager(session=session)
+        umgr = rp.UnitManager(session=session)
+        pd_init = {'resource': 'llnl.lassen',
+                   'runtime': 30,
+                   'exit_on_error': True,
+                   'project': 'cv19-a01',
+                   'access_schema': 'local',
+                   'cores': 40,
+                   'input_staging': ['hello_rp.sh']}
+        pdesc = rp.ComputePilotDescription(pd_init)
+        pilot = pmgr.submit_pilots(pdesc)
+        umgr.add_pilots(pilot)
+        cuds = list()
+        for _num in range(N_TASKS):
+            cuds.append(rp.ComputeUnitDescription({
+                'cpu_processes': 1,
+                'cpu_process_type': 'MPI',
+                'cpu_threads': 2,
+                'cpu_thread_type': 'OpenMPI',
+                'executable': './hello_rp.sh',
+                'input_staging': [
+                    {'source': 'pilot:///hello_rp.sh',
+                     'target': 'unit:///hello_rp.sh',
+                     'action': rp.LINK}],
+                'output_staging': [
+                    {'source': 'unit:///STDOUT',
+                     'target': 'client:///output_hrp_%d.txt' % _num,
+                     'action': rp.TRANSFER}]}))
+        umgr.submit_units(cuds)
+        umgr.wait_units()
+    finally:
+        session.close(download=True)
+```
+
+---
+
+## Amber/AmberTools installation
+- version 20
+```shell script
+# group workspace is used here:
+#  /usr/workspace/cv_ddmd/ (personal dir: /usr/workspace/cv_ddmd/$USER/)
+
+source /p/gpfs1/$USER/.miniconda3/etc/profile.d/conda.sh
+conda activate ve.rp
+
+module load cuda/10.2.89
+
+# get Amber20 -> provided by Arvind
+# get AmberTools20 (based on the approach from WF3)
+wget 'https://ambermd.org/cgi-bin/AmberTools20-get.pl?filename=AmberTools20.tar.bz2&Name=na&Institution=na&City=na&State=na&Country=na' -O AmberTools20.tar.bz2
+tar xfj AmberTools20.tar.bz2
+cd amber20_src/
+ml gcc fftw
+./configure -mpi -cuda -noX11 -nosse -nofftw3 gnu
+make -j 40 install
+
+# usage
+
+source /usr/workspace/cv_ddmd/$USER/amber20_src/amber.sh
+# export LD_LIBRARY_PATH="/usr/tce/packages/cuda/cuda-10.2.89/lib64:${LD_LIBRARY_PATH}"
+```
+- version 19 (following the approach from WF3)
+```shell script
+source /p/gpfs1/$USER/.miniconda3/etc/profile.d/conda.sh
+conda activate ve.rp
+
+module load cuda/10.2.89
+module load gcc/8.3.1
+
+wget 'https://ambermd.org/cgi-bin/AmberTools19-get.pl?filename=AmberTools19.tar.bz2&Name=na&Institution=na&City=na&State=na&Country=na' -O AmberTools19.tar.bz2
+tar xfj AmberTools19.tar.bz2
+cd amber18/
+
+# first part without CUDA
+LANG=en_US.UTF-8 CC=gcc CXX=g++ FC=gfortran ./configure --with-python `which python` -nofftw3 --no-updates -nosse gnu
+make -j 40 install
+
+# now CUDA part
+./configure --with-python `which python` -nofftw3 --no-updates -cuda -nosse gnu
+export LD_LIBRARY_PATH="/usr/tce/packages/cuda/cuda-10.2.89/lib64:${LD_LIBRARY_PATH}"
+make -j 40 install
+
+module load spectrum-mpi/rolling-release
+
+# compile the -mpi version:
+# first part without CUDA
+CC=gcc CXX=g++ FC=gfortran ./configure --with-python `which python` -nofftw3 --no-updates -nosse -mpi gnu
+make -j 40 install
+
+# p.s. note that it is possible to also compile the combination of -mpi and -cuda. 
+# It might be a good idea to have it too: with CUDA
+CC=gcc CXX=g++ FC=gfortran ./configure --with-python `which python` -nofftw3 --no-updates -nosse -mpi -cuda gnu
+make -j 40 install
+
+# usage
+
+source /p/gpfs1/$USER/amber18/amber.sh
+```
+## Pre-run | Set parameters
+
+```shell script
+source /p/gpfs1/$USER/.miniconda3/etc/profile.d/conda.sh
+conda activate ve.rp
+
+cd /p/gpfs1/$USER
+wget https://github.com/openbabel/openbabel/archive/openbabel-3-1-1.tar.gz
+tar zxf openbabel-3-1-1.tar.gz
+mkdir -p ./openbabel-openbabel-3-1-1/build ./openbabel
+cd openbabel-openbabel-3-1-1/build
+cmake ../ -DCMAKE_INSTALL_PREFIX=/p/gpfs1/$USER/openbabel
+make -j 40 install
+```
+```shell script
+export PATH=/p/gpfs1/$USER/openbabel/bin/:$PATH
+```
+```shell script
+cd <WF2>/Parameters/
+python paramterize.py
+```
+
+---
+
+# Pre-run
+```shell script
+source /p/gpfs1/$USER/.miniconda3/etc/profile.d/conda.sh
+conda activate ve.rp
+
+export AMBERHOME=/usr/workspace/cv_ddmd/$USER/amber20_src
+# or export AMBERHOME=/p/gpfs1/$USER/amber18
+```
+**NOTE:** Edit file with credentials `creds_lassen.json`
+
+---
+
+## Additional information
+
+[Resource Guide for New Livermore Computing Users](https://hpc.llnl.gov/training/tutorials/llnl-covid-19-hpc-resource-guide)
+
+### Conda installers
+`/collab/usr/gapps/python/blueos_3_ppc64le_ib_p9/conda/`
+
+### Running jobs
+https://computing.llnl.gov/tutorials/lc_resources/#RunningJobs
+https://hpc.llnl.gov/training/tutorials/using-lcs-sierra-system#Running
+
+### MyLC (dashboard)
+https://lc.llnl.gov/lorenz/mylc/mylc.cgi
